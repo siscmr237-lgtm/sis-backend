3bfde270cd521021372ec5a8d70fec91deb7febc
Randy Kwalar <rkwalar@gmail.com>
2025-11-19 19:04:50 +0100

    save

--- src/db/prisma.js
+++ src/db/prisma.js
@@ -1,5 +1,21 @@
+// const { PrismaClient } = require('@prisma/client');
+
+// const prisma = new PrismaClient();
+
+// module.exports = { prisma };
+
+// prisma.js
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+let prisma = new PrismaClient();
+
+if (process.env.NODE_ENV === 'production') {
+} else {
+  // Use global cache in development to avoid multiple instances
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
 module.exports = { prisma };
